1c8d3681616023490097f768ea572aec5e0af8fc
Tigerking02k <tigerking02k@gmail.com>
2025-03-12 10:16:16 +0530

    ch-12 completed

--- ch-12/98.c
+++ ch-12/98.c
@@ -1,32 +1,32 @@
 // Create an array using calloc and fill it with random numbers, then use realloc to shrink the array size by half and print the remaining numbers.
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     srand(time(NULL));
     int n = 10;
-    int *arr = (int *) malloc(n * sizeof(int));
-    
-    if (arr == NULL) {
-        printf("Memory is unavailable!!! :(");
-        return 1;
-    }
 
+    int *arr = malloc(n * sizeof(int));
+
+    printf("\nHere random numbers:-\n");
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 50;
+        arr[i] = rand() % 100;
         printf("%d\t", arr[i]);
     }
-
+    
     printf("\n");
 
-    n = 5;
-    arr = (int *) realloc(arr, n);
+    n /= 2;
+    arr = realloc(arr, n * sizeof(int));
+
+    printf("\nHere new allocated numbers:-\n");
     for (int i = 0; i < n; i++) {
         printf("%d\t", arr[i]);
     }
-    
+
     free(arr);
     arr = NULL;
+
     return 0;
 }
